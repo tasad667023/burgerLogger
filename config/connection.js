70238df825eb8218ce6b25e5a7329f9d8f2ac794
Tayyaba <tayyaba.asad1234@gmail.com>
2021-01-22 22:41:51 -0500

    updated code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+let connection; 
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'NewPassword',
-  database: 'burgers_db',
-});
+if(process.env.JAWSDB_URL){
+ connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+    connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: 'NewPassword',
+    database: 'burgers_db',
+  });
+}
 
 // Make connection.
 connection.connect((err) => {
@@ -21,3 +27,5 @@ connection.connect((err) => {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
